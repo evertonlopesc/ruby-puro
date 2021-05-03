0523af97896b144aa8902055a85981978e16f18d
Everton Costa <everton.lopess@outlook.com>
2021-05-03 12:58:14 -0300

    Segundo desafio, receber dois numeros e somar, sub, mult e div.

---
+++ aula02/desafio_two.rb
@@ -0,0 +1,11 @@
+print "Digite um número inteiro: "
+a = gets.chomp
+
+print "Digite o segundo número inteiro: "
+b = gets.chomp
+
+puts "A soma entre eles: #{a.to_i + b.to_i}"
+puts "A subtração entre eles: #{a.to_i - b.to_i}"
+puts "A multiplicação entre eles: #{a.to_i * b.to_i}"
+puts "A divisão entre eles: #{a.to_i / b.to_i}"
+puts "A módulo entre eles: #{a.to_i % b.to_i}"
