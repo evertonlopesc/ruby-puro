83bfbb49f3f43bcb3d7d3e833a0a1834657135ac
Everton Costa <everton.lopess@outlook.com>
2021-05-03 13:04:12 -0300

    Iterador times

---
+++ aula03/times.rb
@@ -0,0 +1,12 @@
+
+count = 1
+
+week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+week.size.times {
+    puts "#{count} - #{week}"
+    count += 1
+}
+
+5.times { |i| puts "hello #{i}" }
+5.times { |i| puts "hello #{i + 1}" }
