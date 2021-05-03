d6e44e6dd4f9aad763f92ce2e7aff13c2a0de33d
Everton Costa <everton.lopess@outlook.com>
2021-05-03 12:59:24 -0300

    Condicional if esif else

---
+++ aula03/if_esif_else.rb
@@ -0,0 +1,13 @@
+# CONDITIONAL
+
+day = 'Monday'
+
+if day == 'Sunday'
+    lunch = 'Especial'
+elsif day == 'Saturday'
+    lunch = 'Especial também'
+else
+    lunch = 'normal'
+end
+
+puts "Hoje temos um lanche #{lunch}!"
