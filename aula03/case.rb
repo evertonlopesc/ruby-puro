c0ec1f872151e00e260226321d44ba2a3e950529
Everton Costa <everton.lopess@outlook.com>
2021-05-03 12:59:44 -0300

    Condicional case

---
+++ aula03/case.rb
@@ -0,0 +1,13 @@
+# ITERATION
+mes = "Março"
+
+case mes
+when "Janeiro"
+    puts "Mês de aniversário é em Janeiro"
+when "Fevereiro"
+    puts "Mês de aniversário é em Fevereiro"
+when "Março"
+    puts "Mês de aniversário é em Março"
+else
+    puts "Nenhuma das opições anteriores"
+end
