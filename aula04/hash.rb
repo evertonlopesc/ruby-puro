91621d73f81e65e860eb6357b82ef79fd59f6daf
Everton Costa <everton.lopess@outlook.com>
2021-05-05 06:35:27 -0300

    Como criar hash

---
+++ aula04/hash.rb
@@ -0,0 +1,19 @@
+# HASH
+
+# COMO CRIAR HASH
+print 'COMO CRIAR HASH'
+puts ''
+print course = { "Computacao" => "Ciencia da Computacao", "Gerencia" => "Administracao" }
+puts ''
+print products = { :livro => 150, :caderno => 25 }
+puts ''
+print options = { acordar: 5, transporte: 'onibus' }
+puts ''
+
+
+
+
+
+
+print
+puts ''
