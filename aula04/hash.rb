889640a66f4ee7822db442ca11ef2da26ebd5138
Everton Costa <everton.lopess@outlook.com>
2021-05-05 06:50:50 -0300

    Criando com instancia e declarando como default

--- aula04/hash.rb
+++ aula04/hash.rb
@@ -9,10 +9,12 @@ print products = { :livro => 150, :caderno => 25 }
 puts ''
 print options = { acordar: 5, transporte: 'onibus' }
 puts ''
-
-
-
-
+hash = Hash.new
+print hash["Dorothy Doe"] = 9
+puts ''
+products = Hash.new(0)
+course = { "Financas" => "Contabilidade" }
+course.default = 0
 
 
 print
