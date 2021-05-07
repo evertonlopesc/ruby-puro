050e52041bed088b5169c6a53806331b6080a05f
Everton Costa <everton.lopess@outlook.com>
2021-05-07 06:59:47 -0300

    Refatorando comentários e função

--- aula05/desafio_metodo.rb
+++ aula05/desafio_metodo.rb
@@ -1,20 +1,18 @@
-=begin
- Criar um programa que possua um método 
- para resolver um problema de potenciação
- o usuário deve passar o número base e seu expoente
- imprima na tela o resultado
-    
-=end
+# Criar um programa que possua um método
+# para resolver um problema de potenciação
+# o usuário deve passar o número base e seu expoente
+# imprima na tela o resultado
+
+def potentiation(base = 4, exponent = 2)
+  base**exponent
+end
+
 print 'Digite o número base: '
 number = gets.chomp.to_i
 
 print 'Digite o expoente: '
 exponent = gets.chomp.to_i
 
-def potentiation (numero = 4, exponent = 2)
-  puts "Resultado: #{result = numero ** exponent}"
-end
+potentialization = potentiation(number, exponent)
 
-potentiation(number, exponent)
-
-
+puts "\nO número base #{number} a potência #{exponent}, resulta em: #{potentialization}"
