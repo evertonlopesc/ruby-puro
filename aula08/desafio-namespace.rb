6089c9f340b4128ebfe301ce5c488ec1b1c64931
Everton Costa <everton.lopess@outlook.com>
2021-05-08 09:11:32 -0300

    Desafio Namespace criado

---
+++ aula08/desafio-namespace.rb
@@ -0,0 +1,9 @@
+# Missão 2
+#
+# Crie um módulo chamado Person com as classes Juridic e Physical.
+#
+# Ao executar a instrução
+# 1 Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+#
+# E com a instruçãoPerson::Physical.new('José Almeida', '425.123.123-123').add
+# 1 Person::Physical.new('José Almeida', '425.123.123-123').add
