b8806b883a3a153b0d1d37e8ab13620c5f6e1df6
Everton Costa <everton.lopess@outlook.com>
2021-05-08 12:46:30 -0300

    Desafio Namespace concluido.

--- aula08/desafio-namespace.rb
+++ aula08/desafio-namespace.rb
@@ -5,5 +5,46 @@
 # Ao executar a instrução
 # 1 Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
 #
-# E com a instruçãoPerson::Physical.new('José Almeida', '425.123.123-123').add
-# 1 Person::Physical.new('José Almeida', '425.123.123-123').add
+# E com a instrução
+# 2 Person::Physical.new('José Almeida', '425.123.123-123').add
+
+module Person
+  # Person::Juridic
+  class Juridic
+    def initialize(name, cnpj)
+      @name_juridic = name
+      @cnpj = cnpj
+    end
+
+    def add
+      puts 'Pessoa Jurídica adicionada'
+      puts "nome: #{@name_juridic}"
+      puts "cnpj: #{@cnpj}"
+    end
+  end
+
+  # Person::Phycical
+  class Physical
+    def initialize(name, cpf)
+      @name_physical = name
+      @cpf = cpf
+    end
+
+    def add
+      puts 'Pessoa Física adicionada'
+      puts "nome: #{@name_physical}"
+      puts "cpf: #{@cpf}"
+    end
+  end
+end
+
+result = 2
+
+case result
+when 1
+  Person::Juridic.new('M. C. Investimentos', '4241.123/0001').add
+when 2
+  Person::Physical.new('José Almeida', '425.123.123-123').add
+else
+  puts 'ERROU...!'
+end
