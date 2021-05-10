8de168467e76d1d59b757b30dc05af781fd7cfb5
Everton Costa <everton.lopess@outlook.com>
2021-05-10 06:26:28 -0300

    Desafio Private com regex concluido.

--- aula09/desafio-private-regex.rb
+++ aula09/desafio-private-regex.rb
@@ -5,4 +5,23 @@
 # O método get_km deve chamar um método privado com o nome de find_km.
 # Este deve localizar e retornar o casamento de padrão 80km/h.
 # No final, imprima este retorno.
+class Carro
+  def get_km(text)
+    find_km(text)
+  end
 
+  private
+
+  def find_km(text)
+    puts text
+    print "Busca regex: posição #{/\d{2}\w{2}\/\w/ =~ text}"
+    puts " - #{%r{\d{2}\w{2}/\w}.match(text)}"
+  end
+end
+
+carro = Carro.new
+
+puts carro.get_km('Um fusca de cor amarela viaja a 80km/h')
+
+puts 'Acessando o método private.'
+puts carro.find_km('Um fusca de cor amarela viaja a 80km/h')
